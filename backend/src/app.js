170c5ef668e279c2e8e424a8bcd691758640aecb
Inayathasan0 <inayathasan82@gmail.com>
2025-05-12 00:38:06 +0530

    temporary edit for testing

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,11 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: [
-      "https://lushkart.onrender.com",
-      "https://lushkart-admin.onrender.com",
-      "http://localhost:5173",
-    ],
+    origin: "*",
+    // "https://lushkart.onrender.com",
+    // "https://lushkart-admin.onrender.com",
+    // "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
